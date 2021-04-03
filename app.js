0d5ff196233ccbfb6d5ac4600c9084dbbebb7d59
Valter Garcia <junior.opto@gmail.com>
2021-04-03 11:03:30 -0300

    css done.

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const links = require('./config/variables');
+
 const app = express();
 
 const port = 3000;
 
 app.set('view engine', 'ejs');
 
+app.use(express.static('./public'));
+
 app.get('/', (req, res) =>{
     res.render('landing')
-})
+});
+
+app.get('/whatsapp', (req, res) => {
+    res.redirect(links.subscribe);
+});
+
+app.get('/contact', (req, res) => {
+    res.redirect(links.contact);
+});
+
 
 app.listen(port, () => {
     console.log('listen to port: ' + port);
---
+++ config/variables.js
@@ -0,0 +1,5 @@
+const links = { 
+subscribe: 'https://api.whatsapp.com/send?phone=5517996222349&text=Ol%C3%A1!%20Quero%20me%20inscrever%20no%20curso%20para%20barbeiro%20profissional%20com%20Rafael%20Garcia!',
+contact: 'https://api.whatsapp.com/send?phone=5517996222349&text=Ol%C3%A1!%20Estou%20interessado%20no%20curso%20de%20barbeiro%20profissional%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es!'
+}
+module.exports = links;
